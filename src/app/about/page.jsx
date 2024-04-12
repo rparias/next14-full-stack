f787af6f11dc7a26fdcc956f9acd617d66ec1298
Ronald Arias <ariasron@hotmail.com>
2024-04-12 10:12:24 -0500

    adds images internal and from external sites

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,14 @@
+import Image from "next/image";
 import styles from "./about.module.css"
 
 const AboutPage = () => {
   return (
-    <div className={styles.container}>AboutPage</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/about.png" alt="about" width={500} height={500} />
+        <Image src="https://images.pexels.com/photos/19404767/pexels-photo-19404767/free-photo-of-man-in-jacket-standing-in-mountains.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="about" width={500} height={500} />
+      </div>
+    </div>
   )
 }
 
